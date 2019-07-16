f3b5b50df7612430a9bf0bbd9988c883722e0310
Balkrishna Pandey <balkrishna.pandey@charter.com>
2019-07-16 14:15:51 -0600

    synchronization
    Signed-off-by: Balkrishna Pandey <balkrishna.pandey@charter.com>

---
+++ 009-timeouts/synchronization.go
@@ -0,0 +1,21 @@
+// https://gobyexample.com/channel-synchronization
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	done := make(chan bool, 1)
+	go worker(done)
+	<-done
+}
+
+func worker(done chan bool) {
+	fmt.Println("Working ...")
+	time.Sleep(time.Second)
+	fmt.Println("Done!!!")
+	done <- true
+}
