aded6d5ef01076bcdce74f4c430cf047655a704a
Balkrishna Pandey <balkrishna.pandey@charter.com>
2019-07-22 17:03:41 -0600

    interface animal example
    Signed-off-by: Balkrishna Pandey <balkrishna.pandey@charter.com>

---
+++ gobyexample.com/interfaces/animal.go
@@ -0,0 +1,59 @@
+// https://jordanorelli.com/post/32665860244/how-to-use-interfaces-in-go
+
+package main
+
+import "fmt"
+
+type Animal interface {
+	Speak() string
+}
+
+type Dog struct {
+}
+
+type Cat struct {
+}
+
+type Llama struct {
+}
+
+type JavaProgrammer struct {
+}
+
+func (d Dog) Speak() string {
+	return "Woof!"
+}
+
+func (c Cat) Speak() string {
+	return "Meow!"
+}
+
+func (l Llama) Speak() string {
+	return "????"
+}
+
+func (j JavaProgrammer) Speak() string {
+	return "Design Patterns!"
+}
+
+func AnimalSounds(a Animal) {
+	fmt.Println(a.Speak())
+}
+
+func main() {
+	dog := Dog{}
+	cat := Cat{}
+	llama := Llama{}
+	javaProgrammer := JavaProgrammer{}
+
+	AnimalSounds(dog)
+	AnimalSounds(cat)
+	AnimalSounds(llama)
+	AnimalSounds(javaProgrammer)
+
+	animals := []Animal{Dog{}, Cat{}, Llama{}, JavaProgrammer{}}
+
+	for _, animal := range animals {
+		fmt.Println(animal.Speak())
+	}
+}
